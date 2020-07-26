6e9ae45b5bb3634a6605cc2d4409b39884df34cc
Will Canniford <willcanniford@yahoo.co.uk>
2020-07-26 07:12:43 +0100

    Extend helpers scripts for scraping
    - Create get_innings_by_innings_stats function with switch for batting and bowling

--- scraping_scripts/helpers.py
+++ scraping_scripts/helpers.py
@@ -1,46 +1,69 @@
 # Imports
-import requests
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 
+
 def parse_statistics_table(url, table_caption):
-	'''
-	Find a table of statistics and create pandas DataFrame
+    """
+    Scrape a given table via a caption and store using pandas
 
-	Parameters
-	----------
-	url: string 
-		The URL that shows the table 
-	table_caption: string
-		The text in the table caption
+    Parameters
+    ----------
+    url: str
+        URL link where the table is found
+    table_caption: str
+        HTML caption associated with the table of interest
 
-	Returns
-	-------
-	df: pd.DataFrame
-		DataFrame version of table from url with particular caption
-	'''
-	page_soup = BeautifulSoup(requests.get(url).text,
-                                features="html.parser")
+    Returns
+    -------
+    pandas.DataFrame
+        HTML table as a pandas.DataFrame
+    """
+    page_soup = BeautifulSoup(requests.get(url).text,
+                              features="html.parser")
 
-	# Find main table
-	for caption in page_soup.find_all('caption'):
-	    if caption.get_text() == table_caption:
-	        table = caption.find_parent(
-	            'table', {'class': 'engineTable'})
+    # Find main table
+    for caption in page_soup.find_all('caption'):
+        if caption.get_text() == table_caption:
+            table = caption.find_parent(
+                'table', {'class': 'engineTable'})
+
+    # Isolate the column names <thead>
+    table_thead = table.find('thead').find_all('tr')[0]
+    columns = [x.get_text() for x in table_thead.find_all('th')]
+
+    # Find and store each row
+    rows = []
+
+    for innings in [row for row in table.find('tbody').find_all('tr')]:
+        rows.append([stat.get_text() for stat in innings.find_all('td')])
+
+    # Create pandas data frame and save to csv
+    df = pd.DataFrame(rows, columns=columns).apply(pd.to_numeric, errors='ignore')
+
+    return df
 
 
-	# Isolate the column names <thead>
-	table_thead = table.find('thead').find_all('tr')[0]
-	columns = [x.get_text() for x in table_thead.find_all('th')]
+def get_innings_by_innings_stats(player_id, type):
+    """
+    Return a cleaned version of stats tables for a player
 
-	# Find and store each row
-	rows = []
+    Parameters
+    ----------
+    player_id
+        The ESPN id for the given cricketer
 
-	for innings in [row for row in table.find('tbody').find_all('tr')]:
-	    rows.append([stat.get_text() for stat in innings.find_all('td')])
+    Returns
+        pandas.DataFrame
+    -------
 
-	# Create pandas data frame and save to csv
-	df = pd.DataFrame(rows, columns=columns).apply(pd.to_numeric, errors='ignore')
+    """
+    if type == 'Batting':
+        table_url = f'http://stats.espncricinfo.com/ci/engine/player/{player_id}.html?class=1;template=results;type=batting' \
+                    f';view=innings'
 
-	return(df)
-	
+    elif type == 'Bowling':
+        table_url = f'http://stats.espncricinfo.com/ci/engine/player/{player_id}.html?class=1;template=results;type=bowling;view=innings'
+
+    return parse_statistics_table(url=table_url, table_caption='Innings by innings list')
